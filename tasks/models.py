52426c77da9d876c851cec1aeaa402a6624a9826
Jin Hoon Bang <jinhoonbang@gmail.com>
2016-01-19 17:32:36 -0600

    add urls.py

--- tasks/models.py
+++ tasks/models.py
@@ -4,15 +4,22 @@ from django.db import models
 
 
 class Category(models.Model):
-    title = models.CharField(max_length = 50)
+    cat_title = models.CharField(max_length = 50)
     description = models.CharField(max_length = 200)
+    selected = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.cat_title
 
 class Task(models.Model):
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
-    title = models.CharField(max_length = 50)
+    task_title = models.CharField(max_length = 50)
     content = models.CharField(max_length = 200)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.task_title
 
     # question_text = models.CharField(max_length=200)
     # pub_date = models.DateTimeField('date published')
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request, question_id):
+    categories = Category.objects.all()
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def selected(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
