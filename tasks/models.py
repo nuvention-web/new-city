482552fa2de49dbf419b080837741dcfcb059eb1
Jin Hoon Bang <jinhoonbang@gmail.com>
2016-01-19 16:52:54 -0600

    add Category and Task models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 200)
+
+class Task(models.Model):
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 50)
+    content = models.CharField(max_length = 200)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    # question_text = models.CharField(max_length=200)
+    # pub_date = models.DateTimeField('date published')
+
