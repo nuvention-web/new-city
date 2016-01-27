beacf3956fccd51e6f9ec9e493c15edcb8c9f660
Jin Hoon Bang <jinhoonbang@gmail.com>
2016-01-26 18:11:09 -0600

    add templates category.html and question.html

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Task, Category
+from .models import *
 
 admin.site.register(Task)
 admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Choice)
