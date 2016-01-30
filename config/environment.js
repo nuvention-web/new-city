fbb238e425db6f5e921e64bb24966cefe845418a
sunghyukpark <sunghyukpark0408@gmail.com>
2016-01-30 14:49:58 -0600

    tutorial data structure

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'new-city',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://new-city.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
