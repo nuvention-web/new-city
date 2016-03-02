807c3cc5da0825aec5da639b9cdfc5e241afd1d7
Jin Hoon Bang <jinhoonbang@gmail.com>
2016-03-02 02:26:47 -0600

    drop down functional

--- newcity/views.py
+++ newcity/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from posts.forms import CityForm
+from posts.models import City
 import json
 #from django.contrib.formtools.wizard.views import SessionWizardView
 from formtools.wizard.views import SessionWizardView
 from django.http import HttpResponseRedirect
 
+
+
 # Create your views here.
 def home(request, initial_city=None):
     form = CityForm(request.GET or None)
+    city_list = City.objects.all()
 
     if form.is_valid():
         print("here")
@@ -29,5 +33,8 @@ def home(request, initial_city=None):
         elif 'searching_for_roommate' in request.GET:
             return HttpResponseRedirect(reverse('posts:list_roommate'))
 
-    context = {}
+    context = {
+        "city_list" : city_list,
+    }
+
     return render(request, 'HP.html', context)
