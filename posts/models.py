10e6ea445d8644e5ff835281d52f130cb2ce7e38
Jin Hoon Bang <jinhoonbang@gmail.com>
2016-03-02 21:07:12 -0600

    add budget to model

--- posts/models.py
+++ posts/models.py
@@ -43,6 +43,7 @@ class UserProfile(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER, null=False)
     school = models.CharField(max_length=50)
     hometown = models.ForeignKey(City, null=True, blank=True, related_name = "hometown")
+    budget = models.PositiveIntegerField(default=0)
     job = models.CharField(max_length=50)
     birthday = models.DateField(error_messages={'invalid': "Please enter a correct date format"}, null=True, blank=True)
     picture = models.BinaryField(null=True)
